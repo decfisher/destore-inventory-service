4abcb9ecc7883287eb9f15f5b16e5545c3a93df9
Declan Fisher <decfishe@amazon.com>
2024-08-14 16:29:32 +0100

    feat: Adding basic inventory management functionality

--- src/index.ts
+++ src/index.ts
@@ -69,6 +69,269 @@ app.post('/products/add', async (req: Request<{}, {}, AddProductRequest, {}>, re
   }
 });
 
+app.patch('/add-stock', async (req: Request, res: Response) => {
+  const {
+    id,
+    quantity,
+  } = req.body;
+
+  try {
+    const product = await Product.findOne({ _id: id });
+
+    if (!product) {
+      return res.status(404).json({
+        code: 404,
+        service: SERVICE_NAME,
+        route: '/add-stock',
+        error: 'Product not found',
+      });
+    }
+
+    if (quantity < 1) {
+      return res.status(400).json({
+        code: 400,
+        service: SERVICE_NAME,
+        route: '/add-stock',
+        error: 'Specifiy a quantity larger than 0',
+      });
+    }
+
+    const updatedQuantity = product.quantity + quantity
+
+    await product.updateOne({ quantity: updatedQuantity });
+
+    res.status(200).json({
+      code: 200,
+      service: SERVICE_NAME,
+      route: '/add-stock',
+      product: {
+        id: product._id,
+        name: product.name,
+        quantity: product.quantity,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      service: SERVICE_NAME,
+      route: '/add-stock',
+      error: 'Failed to update product stock',
+    });
+  }
+});
+
+app.patch('/remove-stock', async (req: Request, res: Response) => {
+  const {
+    id,
+    quantity,
+  } = req.body;
+
+  try {
+    const product = await Product.findOne({ _id: id });
+
+    if (!product) {
+      return res.status(404).json({
+        code: 404,
+        service: SERVICE_NAME,
+        route: '/remove-stock',
+        error: 'Product not found',
+      });
+    }
+
+    if (quantity < 1) {
+      return res.status(400).json({
+        code: 400,
+        service: SERVICE_NAME,
+        route: '/remove-stock',
+        error: 'Specifiy a quantity larger than 0',
+      });
+    }
+
+    const updatedQuantity = product.quantity! - quantity
+
+    if (updatedQuantity < 0) {
+      return res.status(400).json({
+        code: 400,
+        service: SERVICE_NAME,
+        route: '/remove-stock',
+        error: 'Not enough stock',
+      });
+    }
+
+    await product.updateOne({ quantity: updatedQuantity });
+
+    res.status(200).json({
+      code: 200,
+      service: SERVICE_NAME,
+      route: '/remove-stock',
+      product: {
+        id: product._id,
+        name: product.name,
+        quantity: product.quantity,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      service: SERVICE_NAME,
+      route: '/remove-stock',
+      error: 'Failed to update product stock',
+    });
+  }
+});
+
+app.get('/find/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findOne({ _id: id });
+
+    if (!product) {
+      return res.status(404).json({
+        code: 404,
+        service: SERVICE_NAME,
+        route: '/find',
+        error: 'Product not found',
+      });
+    }
+
+    res.status(200).json({
+      code: 200,
+      service: SERVICE_NAME,
+      route: '/find',
+      product: {
+        id: product._id,
+        name: product.name,
+        quantity: product.quantity,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      service: SERVICE_NAME,
+      route: '/find',
+      error: 'Failed to find product',
+    });
+  }
+});
+
+app.get('/all', async (req: Request, res: Response) => {
+  try {
+    const result = await Product.find();
+
+    const products = result.map(record => {
+      return {
+        id: record._id,
+        name: record.name,
+        quantity: record.quantity,
+        price: record.price,
+        description: record.description,
+        createdAt: record.createdAt,
+        updatedAt: record.updatedAt,
+      }
+    });
+
+    res.status(200).json({
+      code: 200,
+      service: SERVICE_NAME,
+      route: '/all',
+      products: products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      service: SERVICE_NAME,
+      route: '/all',
+      error: 'Failed to find products',
+    });
+  }
+});
+
+app.delete('/remove/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findOne({ _id: id });
+
+    if (!product) {
+      return res.status(404).json({
+        code: 404,
+        service: SERVICE_NAME,
+        route: '/remove',
+        error: 'Product not found',
+      });
+    }
+
+    await Product.deleteOne({ _id: product.id });
+
+    res.status(200).json({
+      code: 200,
+      service: SERVICE_NAME,
+      route: '/all',
+      message: 'Successfully deleted',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      service: SERVICE_NAME,
+      route: '/remove',
+      error: 'Failed to remove product',
+    });
+  }
+});
+
+app.patch('/price/update', async (req: Request, res: Response) => {
+  const { id, price } = req.body;
+
+  try {
+    const product = await Product.findOneAndUpdate(
+      { _id: id },
+      {
+          price: price,
+          updatedAt: Date.now(),
+      },
+      { returnDocument: 'after', },
+    );
+
+    if (!product) {
+      return res.status(404).json({
+        code: 404,
+        service: SERVICE_NAME,
+        route: '/price/update',
+        error: 'Product not found',
+      });
+    }
+
+    res.status(200).json({
+      code: 200,
+      service: SERVICE_NAME,
+      route: '/price/update',
+      product: {
+        id: product._id,
+        name: product.name,
+        quantity: product.quantity,
+        price: product.price,
+        description: product.description,
+        createdAt: product.createdAt,
+        updatedAt: product.updatedAt,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      service: SERVICE_NAME,
+      route: '/price/update',
+      error: 'Failed to update product price',
+    });
+  }
+})
+
 // Start the server
 app.listen(port, () => {
   console.log(`🛜 Inventory service running on port ${port}...`);
